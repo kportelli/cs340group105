e43840d8643c5726602be57e090b9f54e7d7b61b
alaaddin hiefield <alaaddin.hiefield@gmail.com>
2024-05-09 22:04:32 -0600

    finished gardener sql

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -33,3 +33,27 @@ SELECT plantID, varietyName, type, price FROM Plants INNER JOIN PlantsPlots ON P
 DELETE FROM PlantsPlots WHERE plantPlotID = :plantPlotID
 
 -- Gardeners
+-- -- -- add a new gardener into `gardeners` table
+INSERT INTO Gardeners (fname, lname, streetAddress, city, zip, phone, email) VALUES (:fname, :lname, :streetAddress, :city, :zip, :phone, :email)
+
+-- -- -- get all gardeners
+SELECT gardenerID, fname, lname, streetAddress, city, zip, phone, email FROM Gardeners
+
+-- -- -- get all plants a gardener is responsible for
+SELECT Plants.varietyName, Plants.type, PlantsPlots.plantsPlotsID
+FROM Gardeners
+INNER JOIN PlotsGardeners ON Gardeners.gardenerID = PlotsGardeners.gardenerID
+INNER JOIN PlantsPlots ON PlotsGardeners.plotID = PlantsPlots.plotID
+INNER JOIN Plants ON PlantsPlots.plantID = Plants.plantID
+WHERE Gardeners.gardenerID = :gardenerID;
+
+-- -- -- get a list of all invoices belonging to a gardener
+SELECT invoiceID, gardenerID, totalCost FROM Invoices WHERE gardenerID = :gardenerID
+
+-- -- -- update a gardener's attributes based on provided gardenerID
+UPDATE Gardeners SET fname = :fname, lname = :lname, streetAddress = :streetAddress, city = :city, zip = :zip, phone = :phone, email = :email WHERE gardenerID = :gardenerID
+
+-- -- -- delete a gardener based on provided gardenerID (and cascade delete all plots the gardener is responsible for)
+DELETE FROM Gardeners WHERE gardenerID = :gardenerID
+-- -- -- -- update Gardener's invoices to show GardenerID as NULL
+UPDATE Invoices SET gardenerID = NULL WHERE gardenerID = :gardenerID
