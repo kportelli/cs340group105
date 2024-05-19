0355569e81f7c4f6af0b87ba242f62e1345d23a3
alaaddin hiefield <alaaddin.hiefield@gmail.com>
2024-05-19 12:14:38 -0600

    finished step 1 and added gitignore file

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@
 /*
     SETUP
 */
+var db      = require('./database/db-connector');
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
 PORT        = 9124;                 // Set a port number at the top so it's easy to change in the future
@@ -12,8 +13,38 @@ PORT        = 9124;                 // Set a port number at the top so it's easy
 */
 app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
     {
+
+        // query1 = 'SELECT * FROM Plants;';
+        // db.pool.query(query1, function(err, results, fields){
+        //     // Send the results to the browser
+        //     res.send(JSON.stringify(results));
+        // });
         res.send("The server is running!")      // This function literally sends the string "The server is running!" to the computer
     });                                         // requesting the web site.
+
+
+    app.get('/plants', (req, res) => {
+        db.pool.getConnection((err, connection) => {
+            if (err) {
+                console.error('Error connecting to the database:', err);
+                res.status(500).send('Internal Server Error');
+                return;
+            }
+    
+            connection.query('SELECT * FROM Plants', (queryErr, results) => {
+                connection.release(); // Release the connection back to the pool
+    
+                if (queryErr) {
+                    console.error('Error executing query:', queryErr);
+                    res.status(500).send('Internal Server Error');
+                    return;
+                }
+    
+                res.json(results); // Send the query results as a JSON response
+            });
+        });
+    });
+
 
 /*
     LISTENER
