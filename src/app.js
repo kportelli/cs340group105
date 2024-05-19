249bd00834f3dd94323220152d54c5b9f2f59884
alaaddin hiefield <alaaddin.hiefield@gmail.com>
2024-05-19 15:53:59 -0600

    complete step 4

--- src/app.js
+++ src/app.js
@@ -7,13 +7,15 @@ var db      = require('./database/db-connector');
 
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 PORT        = 9124;                 // Set a port number at the top so it's easy to change in the future
 
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');     // Import express-handlebars
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
-
 
 /*
     ROUTES
@@ -39,6 +41,50 @@ app.get('/', function(req, res)
     //     // res.json(results); // Send the query results as a JSON response
     // });             
 });                                         // will process this file, before sending the finished HTML to the client.                                      // requesting the web site.
+
+app.post('/add-plant-ajax', function(req, res){
+
+    let data = req.body;
+    console.log(data);
+
+    // capture NULL values
+    let price = parseFloat(data.price);
+    if (isNaN(price)){
+        price = 'NULL';
+    }
+
+    query1 = `INSERT INTO plants (varietyName, type, price) VALUES ('${data.varietyName}', '${data.type}', ${price})`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT * on bsg_people
+            query2 = `SELECT * FROM Plants;`;
+            db.pool.query(query2, function(error, rows, fields){
+
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
 
 
 app.get('/plants', (req, res) => {
