72b860dd76886fe7a7e0a01c0dd22ad162570211
alaaddin hiefield <alaaddin.hiefield@gmail.com>
2024-05-31 01:15:21 -0600

    finished plots

--- src/routes/plots.js
+++ src/routes/plots.js
@@ -31,75 +31,41 @@ router.post('/add-plot-ajax', function (req, res) {
     });
 });
 
-router.put('/put-plot-ajax', function (req, res, next) {
-    let data = req.body;
-
-    let gardenID = parseInt(data.gardenID);
-    let gardenName = data.gardenName;
-    let streetAddress = data.streetAddress;
-    let city = data.city;
-    let zip = data.zip;
-
-    // if gardenID is not a number, send a 400 status code
-    if (isNaN(gardenID)) {
-        res.sendStatus(400);
-        return;
-    }
-
-    let queryUpdateGarden = `UPDATE Gardens SET gardenName = ?, streetAddress = ?, city = ?, zip = ? WHERE gardenID = ?`;
-    let selectUpdatedGarden = `SELECT * FROM Gardens WHERE gardenID = ?`
-
-    // Run the 1st query
-    db.pool.query(queryUpdateGarden, [gardenName, streetAddress, city, zip, gardenID], function (error, rows, fields) {
-        if (error) {
-
-            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
-            console.log(error);
-            res.sendStatus(400);
-        }
-
-        // If there was no error, we run our second query and return that data so we can use it to update the people's
-        // table on the front-end
-        else {
-            // Run the second query
-            db.pool.query(selectUpdatedGarden, [gardenID], function (error, rows, fields) {
-
-                if (error) {
-                    console.log(error);
-                    res.sendStatus(400);
-                } else {
-                    res.send(rows);
-                }
-            })
-        }
-    })
-});
-
 // DELETE
 
 router.delete('/delete-plot-ajax/', function (req, res, next) {
     let data = req.body;
-    let gardenID = parseInt(data.id);
-    let deleteGardenFromGardens = `DELETE FROM Gardens WHERE gardenID = ?`;
-    let deleteGardenFromPlots = `DELETE FROM Plots WHERE gardenID = ?`;
+    let plotID = parseInt(data.plotID);
+    let deleteFromPlotsGardeners = `DELETE FROM PlotsGardeners WHERE plotID = ?`;
+    let deleteFromPlantsPlots = `DELETE FROM PlantsPlots WHERE plotID = ?`;
+    let deleteFromPlots = `DELETE FROM Plots WHERE plotID = ?`;
 
-    db.pool.query(deleteGardenFromPlots, [gardenID], function (error, rows, fields) {
+    db.pool.query(deleteFromPlantsPlots, [plotID], function (error, rows, fields) {
         if (error) {
             // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
             console.log(error);
             res.sendStatus(400);
         }
         else {
-            db.pool.query(deleteGardenFromGardens, [gardenID], function (error, rows, fields) {
+            db.pool.query(deleteFromPlotsGardeners, [plotID], function (error, rows, fields) {
                 if (error) {
                     // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
                     console.log(error);
                     res.sendStatus(400);
                 }
                 else {
-                    //  Since we are just deleting 1 row and don't need to send back any new data,
-                    // we will send back a status of 204 (No Content) common for PUT or DELETE.
-                    res.sendStatus(204);
+                    db.pool.query(deleteFromPlots, [plotID], function (error, rows, fields) {
+                        if (error) {
+                            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                            console.log(error);
+                            res.sendStatus(400);
+                        }
+                        else {
+                            //  Since we are just deleting 1 row and don't need to send back any new data,
+                            // we will send back a status of 204 (No Content) common for PUT or DELETE.
+                            res.sendStatus(204);
+                        }
+                    });
                 }
             });
         }
